f272a61f933f5584f85109b4d219ac2ff82f7a32
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-04-17 19:30:44 +0200

    adding custom middlewares

--- app.js
+++ app.js
@@ -11,6 +11,19 @@ app.listen(3000);
 
 app.set("view engine", "ejs");
 
+app.use((req, res,next) => {
+  console.log("new request made");
+  console.log("host", req.hostname);
+  console.log("path", req.path);
+  console.log("method", req.method);
+  next();
+});
+
+app.use((req,res,next) => {
+  console.log("in the next middleware");
+  next();
+})
+
 app.get("/", (req, res) => {
   const blogs = [
     {
